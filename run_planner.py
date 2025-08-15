15064932a018c04c46119df2c75073da9e86bfb8
Kareem Mohamed <kareemmagdy420@gmail.com>
2025-08-15 22:30:48 +0200

    add the option to plan for 1 scenario

--- run_planner.py
+++ run_planner.py
@@ -25,8 +25,13 @@ from commonroad_rp.utility.evaluation import run_evaluation
 from commonroad_rp.utility.config import ReactivePlannerConfiguration
 
 from commonroad_rp.utility.logger import initialize_logger
+import argparse
+import time
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--scenario", type=str, default="", help="scenario name to run on 1 scenario only")
 
+args = parser.parse_args()
 # *************************************
 # Set Configurations
 # *************************************
@@ -42,69 +47,128 @@ scenarios_dir = "../cvae/scenarios"
 # Initialize Planner
 # *************************************
 # run route planner and add reference path to config
-for sc in os.listdir(scenarios_dir):
-      if sc.endswith(".xml"):            
-            
-            print(f"Planning for {sc}")
-            
-            config = ReactivePlannerConfiguration.load(config_file, sc)
-            config.update()
 
-            try:
-                  # run route planner
-                  route_planner = RoutePlanner(config.scenario, config.planning_problem)
-                  route = route_planner.plan_routes().retrieve_first_route()
+time_start = time.time()
+if args.scenario:
+      sc = args.scenario
+      print(f"Planning for {sc}")
+      config = ReactivePlannerConfiguration.load(config_file, sc)
+      config.update()
+      
+      try:
+            # run route planner
+            route_planner = RoutePlanner(config.scenario, config.planning_problem)
+            route = route_planner.plan_routes().retrieve_first_route()
+                  
+            # get reference path
+            reference_path = route.reference_path
+            
+            planner = ReactivePlanner(config=config)
+
+            # set reference path for curvilinear coordinate system
+            planner.set_reference_path(route.reference_path)
+            while not planner.goal_reached():
+                  current_count = len(planner.record_state_list)
+
+                  planner.set_desired_velocity(current_speed=planner.x_0.velocity)
+
+                  # call plan function
+                  optimal, _ = planner.plan()
+
+                  # record planned state and input
+                  planner.record_state_and_input(optimal[0].state_list[1])
+
+                  # reset planner state for re-planning
+                  planner.reset(initial_state_cart=planner.record_state_list[-1], 
+                              initial_state_curv=(optimal[2][1], optimal[3][1]),
+                              collision_checker=planner.collision_checker, 
+                              coordinate_system=planner.coordinate_system)
+                  
+                  # Create ego vehicle and sampled trajectory bundle for visualization
+                  if config.debug.show_plots or config.debug.save_plots:
+                        ego_vehicle = planner.convert_state_list_to_commonroad_object(optimal[0].state_list)
+                        sampled_trajectory_bundle = None
+                        if config.debug.draw_traj_set:
+                              sampled_trajectory_bundle = deepcopy(planner.stored_trajectories)
+                              
+                        # visualize the current time step of the simulation
+                  if config.debug.show_plots or config.debug.save_plots:
+                        visualize_planner_at_timestep(scenario=config.scenario, planning_problem=config.planning_problem,
+                                                      ego=ego_vehicle, traj_set=sampled_trajectory_bundle,
+                                                      ref_path=planner.reference_path, timestep=current_count, config=config)
+            
+            # save sampled variables and conditiobned variables if scenario is successfully planned
+            if planner.goal_reached():
+                  print(colored(f"Scenario {sc} successfully planned!", "green"))
+                  print(f"Planning took {time.time() - time_start} seconds")
+                  # avg_encode = sum(planner.sampling_space.cvae_helper._times) /\
+                  # len(planner.sampling_space.cvae_helper._times)
+                  # print(f"Average time to encode 1 scenario image is {avg_encode} seconds")
+                  # print(f"Number of samples: {planner.record_state_list[-1].time_step}")
+                  # save_scenario_imgs(sc[:-4], planner.record_state_list[-1].time_step)
+                  # make gif
+                  make_gif(config, range(0, planner.record_state_list[-1].time_step))
+
+      except Exception as e:
+            print(colored(f"Scenario {sc} failed!", "red"))
+            print(f"Error: {e}")
+
+else:
+      for sc in os.listdir(scenarios_dir):
+            if sc.endswith(".xml"):            
+                  
+                  print(f"Planning for {sc}")
+                  
+                  config = ReactivePlannerConfiguration.load(config_file, sc)
+                  config.update()
+
+                  try:
+                        # run route planner
+                        route_planner = RoutePlanner(config.scenario, config.planning_problem)
+                        route = route_planner.plan_routes().retrieve_first_route()
+                              
+                        # get reference path
+                        reference_path = route.reference_path
                         
-                  # get reference path
-                  reference_path = route.reference_path
-                  
-                  planner = ReactivePlanner(config=config)
+                        planner = ReactivePlanner(config=config)
 
-                  # set reference path for curvilinear coordinate system
-                  planner.set_reference_path(route.reference_path)
-                  while not planner.goal_reached():
+                        # set reference path for curvilinear coordinate system
+                        planner.set_reference_path(route.reference_path)
+                        while not planner.goal_reached():
+                              current_count = len(planner.record_state_list) - 1
+                              print(current_count)
 
-                        planner.set_desired_velocity(current_speed=planner.x_0.velocity)
+                              planner.set_desired_velocity(current_speed=planner.x_0.velocity)
 
-                        # call plan function
-                        optimal, _ = planner.plan()
+                              # call plan function
+                              optimal, _ = planner.plan()
 
-                        # record planned state and input
-                        planner.record_state_and_input(optimal[0].state_list[1])
+                              # record planned state and input
+                              planner.record_state_and_input(optimal[0].state_list[1])
 
-                        # reset planner state for re-planning
-                        planner.reset(initial_state_cart=planner.record_state_list[-1], 
-                                    initial_state_curv=(optimal[2][1], optimal[3][1]),
-                                    collision_checker=planner.collision_checker, 
-                                    coordinate_system=planner.coordinate_system)
-                  
-                  # save sampled variables and conditiobned variables if scenario is successfully planned
-                  if planner.goal_reached():
-                        print(colored(f"Scenario {sc} successfully planned!", "green"))
-                        # print(f"Number of samples: {planner.record_state_list[-1].time_step}")
-                        # save_scenario_imgs(sc[:-4], planner.record_state_list[-1].time_step)
+                              # reset planner state for re-planning
+                              planner.reset(initial_state_cart=planner.record_state_list[-1], 
+                                          initial_state_curv=(optimal[2][1], optimal[3][1]),
+                                          collision_checker=planner.collision_checker, 
+                                          coordinate_system=planner.coordinate_system)
+                        
+                        # save sampled variables and conditiobned variables if scenario is successfully planned
+                        if planner.goal_reached():
+                              print(colored(f"Scenario {sc} successfully planned!", "green"))
+                              # print(f"Number of samples: {planner.record_state_list[-1].time_step}")
+                              # save_scenario_imgs(sc[:-4], planner.record_state_list[-1].time_step)
 
-            except Exception as e:
-                  print(colored(f"Scenario {sc} failed!", "red"))
-                  print(f"Error: {e}")
+                  except Exception as e:
+                        print(colored(f"Scenario {sc} failed!", "red"))
+                        print(f"Error: {e}")
 
-                  continue
+                        continue
 
     # print(f"current time step: {current_count}")
-
-    # visualize the current time step of the simulation
-    # if config.debug.show_plots or config.debug.save_plots:
-    #     visualize_planner_at_timestep(scenario=config.scenario, planning_problem=config.planning_problem,
-    #                                   ego=ego_vehicle, traj_set=sampled_trajectory_bundle,
-    #                                   ref_path=planner.reference_path, timestep=current_count, config=config)
-
-# make gif
-# make_gif(config, range(0, planner.record_state_list[-1].time_step))
-
 
 # **************************
 # Evaluate results
 # **************************
-evaluate = True
+evaluate = False
 if evaluate:
     cr_solution, feasibility_list = run_evaluation(planner.config, planner.record_state_list, planner.record_input_list)
